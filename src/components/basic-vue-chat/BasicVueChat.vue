e2887efee3f0fc1d18b8d332cd56897de0ee32dd
Jędrzej Maczan <jedrzejmaczan@gmail.com>
2018-10-14 19:33:55 +0200

    Added auto scroll

--- src/components/basic-vue-chat/BasicVueChat.vue
+++ src/components/basic-vue-chat/BasicVueChat.vue
@@ -4,45 +4,57 @@
       <header class="window__header__container">
         {{ title }}
       </header>
-      <section class="window__messages__container">
-        <div
-          v-for="message in feed"
-          :key="messageKey(message)">
+      <section
+        id="window__messages__container"
+        class="window__messages__container"
+        style="transition: all 0.5s;">
+        <transition-group
+          name="messages-list"
+          tag="div" >
           <div
-            v-if="message.id === mockData.authorId"
-            class="message--own">
-            <div class="message__date">
-              {{ message.date }}
+            v-for="message in feed"
+            :key="messageKey(message)"
+            class="messages-list-item">
+            <div
+              v-if="message.id === mockData.authorId"
+              class="message--own">
+              <div class="message__date">
+                {{ message.date }}
+              </div>
+              <div class="message__contents">
+                {{ message.contents }}
+              </div>
             </div>
-            <div class="message__contents">
-              {{ message.contents }}
+            <div
+              v-else
+              class="message--foreign">
+              <div class="message__author">
+                {{ message.author }}
+              </div>
+              <div class="message__contents">
+                {{ message.contents }}
+              </div>
+              <div class="message__date">
+                {{ message.date }}
+              </div>
             </div>
           </div>
-          <div
-            v-else
-            class="message--foreign">
-            <div class="message__author">
-              {{ message.author }}
-            </div>
-            <div class="message__contents">
-              {{ message.contents }}
-            </div>
-            <div class="message__date">
-              {{ message.date }}
-            </div>
-          </div>
-        </div>
+        </transition-group>
       </section>
       <div class="window__input__container">
-        <div class="input__field">
-          <input
-            type="text"
-            name="message"
-            aria-placeholder="Type message..."
-            placeholder="Type message..."><br>
-        </div>
-        <div class="input__button">
-          Send
+        <div class="input__container">
+          <div class="input__field">
+            <input
+              type="text"
+              name="message"
+              aria-placeholder="Type message..."
+              placeholder="Type message..."><br>
+          </div>
+          <div
+            class="input__button"
+            @click="add">
+            Send
+          </div>
         </div>
       </div>
     </section>
@@ -57,49 +69,63 @@ export default {
       type: String,
       default: 'Team Maczan',
       required: false
-    },
-    feed: {
-      type: Array,
-      default: function () {
-        return [
-          {
-            id: 0,
-            author: 'Gruby',
-            contents: 'hi there',
-            date: '16:30'
-          },
-          {
-            id: 1,
-            author: 'Chudy',
-            contents: 'hello',
-            date: '16:30'
-          },
-          {
-            id: 1,
-            author: 'Chudy',
-            contents: 'lol',
-            date: '16:31'
-          }
-        ]
-      },
-      required: false
     }
   },
   data: function () {
     return {
       mockData: {
         authorId: 1
-      }
+      },
+      feed: [
+        {
+          id: 0,
+          author: 'Gruby',
+          contents: 'hi there',
+          date: '16:30'
+        },
+        {
+          id: 1,
+          author: 'Chudy',
+          contents: 'hello',
+          date: '16:30'
+        },
+        {
+          id: 1,
+          author: 'Chudy',
+          contents: 'lol',
+          date: '16:31'
+        }
+      ]
     }
   },
   methods: {
     messageKey (message) {
       return message.contents + message.date
+    },
+    add () {
+      this.feed.push({ id: 0, author: 'Gruby', contents: 'xd', date: '16:66' })
+
+      setTimeout(function () {
+        var scrollContainer = document.getElementById('window__messages__container')
+        var isScrolledToBottom = scrollContainer.scrollHeight - scrollContainer.clientHeight <= scrollContainer.scrollTop + 1
+        if (!isScrolledToBottom) { scrollContainer.scrollTop = scrollContainer.scrollHeight }
+      }, 500)
     }
   }
 }
 </script>
 
 <style lang="scss">
+.messages-list-item {
+  transition: all 0.5s;
+}
 
+.messages-list-enter, .messages-list-leave-to
+/* .messages-list-leave-active below version 2.1.8 */ {
+  opacity: 0;
+  transform: translateY(30px);
+}
+.messages-list-leave-active {
+  position: absolute;
+}
 </style>
