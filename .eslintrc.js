5d7b87c7257467a67a2ae86259b10e1629806b08
Jędrzej Maczan <jedrzejmaczan@gmail.com>
2018-10-14 16:17:45 +0200

    Project initialized, created basic chat template

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,8 +4,8 @@ module.exports = {
     node: true,
   },
   extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
+    'plugin:vue/recommended',
+    '@vue/standard',
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
--- src/components/basic-vue-chat/BasicVueChat.vue
+++ src/components/basic-vue-chat/BasicVueChat.vue
@@ -1,12 +1,72 @@
 <template>
-  
+  <div class="basic-vue-chat">
+    <section class="window">
+      <header class="window__header__container">
+        Team Maczan
+      </header>
+      <section class="window__messages__container">
+        <div class="message--foreign">
+          <div class="message__author">
+            {{ feed[0].author }}
+          </div>
+          <div class="message__contents">
+            {{ feed[0].contents }}
+          </div>
+          <div class="message__date">
+            {{ feed[0].date }}
+          </div>
+        </div>
+        <div class="message--own">
+          <div class="message__date">
+            {{ feed[1].date }}
+          </div>
+          <div class="message__contents">
+            {{ feed[1].contents }}
+          </div>
+        </div>
+      </section>
+      <div class="window__input__container">
+        <div class="input__field">
+          <input type="text" name="message" aria-placeholder="Type message..." placeholder="Type message..."><br>
+        </div>
+        <div class="input__button">
+          Send
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'BasicVueChat',
   props: {
-    
+    feed: {
+      type: Array,
+      default: function () {
+        return [
+          {
+            id: 0,
+            author: 'Gruby',
+            contents: 'hi there',
+            date: '16:30'
+          },
+          {
+            id: 1,
+            author: 'Chudy',
+            contents: 'hello',
+            date: '16:30'
+          },
+          {
+            id: 1,
+            author: 'Chudy',
+            contents: 'lol',
+            date: '16:31'
+          }
+        ]
+      },
+      required: false
+    }
   }
 }
 </script>
