003cd88d46d372ba2d32f1d5e882c5de5b808f65
jmaczan <jedrzejmaczan@gmail.com>
2018-10-29 13:11:32 +0100

    Prepared code for being a library

---
+++ src/components/basic-vue-chat/entry.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import BasicVueChat from './BasicVueChat'
+
+const Components = {
+  BasicVueChat
+}
+
+Object.keys(Components).forEach(name => {
+  Vue.component(name, Components[name])
+})
+
+export default Components
